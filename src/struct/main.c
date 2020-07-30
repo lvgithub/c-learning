8492b4bf26c673849afe8b1c21904a61ed788c66
flyaway <jolvmail@gmail.com>
2020-07-30 09:19:26 +0800

    ADD: STRUCT

---
+++ src/struct/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Books
+{
+    char title[50];
+    char author[50];
+    char subject[100];
+    int book_id;
+};
+
+int main()
+{
+    struct Books Book1; /* 声明 Book1，类型为 Books */
+    struct Books Book2; /* 声明 Book2，类型为 Books */
+
+    /* Book1 详述 */
+    strcpy(Book1.title, "C Programming");
+    strcpy(Book1.author, "Nuha Ali");
+    strcpy(Book1.subject, "C Programming Tutorial");
+    Book1.book_id = 6495407;
+
+    char tt=
+
+    /* 输出 Book1 信息 */
+    printf("Book 1 title : %s\n", Book1.title);
+    printf("Book 1 author : %s\n", Book1.author);
+    printf("Book 1 subject : %s\n", Book1.subject);
+    printf("Book 1 book_id : %d\n", Book1.book_id);
+
+    return 0;
+}
